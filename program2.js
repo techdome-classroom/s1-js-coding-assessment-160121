a175cf45fafe6857274d7627cfa01abfc0f60648
Navya sree Marepalli <marepallinavyasree@gmail.com>
2024-11-11 13:27:01 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -4,20 +4,20 @@ const decodeTheRing = function (s, p) {
   const dp = Array(sLen + 1)
     .fill(null)
     .map(() => Array(pLen + 1).fill(false));
-
   dp[sLen][pLen] = true;
+
   for (let i = sLen; i >= 0; i--) {
     for (let j = pLen - 1; j >= 0; j--) {
       const firstMatch = i < sLen && (p[j] === s[i] || p[j] === '?');
-
       if (p[j] === '*') {
         dp[i][j] = dp[i][j + 1] || (i < sLen && dp[i + 1][j]);
       } else {
-
         dp[i][j] = firstMatch && dp[i + 1][j + 1];
       }
     }
   }
+
+  return dp[0][0];
 };
 
 module.exports = decodeTheRing;
